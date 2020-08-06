2e647160074c567691bfcfd1ef99473bc728e4ca
Taimas <urazgalit@gmail.com>
2020-08-06 13:57:56 +0600

    1st app v2

--- appOne.js
+++ appOne.js
@@ -1,6 +1,5 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?', '');
-console.log(numberOfFilms);
-
+//Часть 1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,14 +7,32 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count  >= 10 &&  personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 console.log(personalMovieDB['count']);
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько оцените его?');
+//Часть 2
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой последний фильм вы посмотрели ?');
+    const b = prompt('На сколько вы его оцените ?');
+    
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('sucsess');
+} else {
+    console.log('error');
+    i--;
+}
 
+}
 console.log(personalMovieDB);
+
+
---
+++ cycles.js
@@ -0,0 +1,24 @@
+'use strict';
+ 
+
+
+// while (num <= 100) {
+    //     console.log(num);
+    //     num++;
+    // }
+    
+    
+    // do {
+        // console.log(num);
+        // num++;
+        // }
+        // while (num <= 50);
+        
+// let num = prompt('Введите число');
+for (let i = 1; i <= 20; i++) {
+    if (i === 6){
+        // break;
+        continue;
+    }
+    console.log(i);
+}
