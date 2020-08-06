fb055ddad8fab262706e8447e1fad15c91e86c75
Taimas <urazgalit@gmail.com>
2020-08-06 11:28:04 +0600

    new console app

---
+++ conditions.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// if (4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error')
+// }
+
+// if (num < 49) {
+//     console.log('error');
+    
+// } else if (num > 100) {
+//     console.log('Mnogo');
+// } else {
+//     console.log('OK')
+// }
+
+// (num == 50) ? console.log('OK') :  console.log('error'); // Тернарные
+//операторы
+
+
+
+const num = 51;
+switch (num) {
+    case 49:
+        console.log('Neverno');
+        break;
+    case 100: 
+        console.log('neverno');
+        break;
+    case 50:
+        console.log('v tochku');
+        break;
+    default:
+        console.log('NET');
+        break;
+
+
+}
---
+++ js/script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// let number = 45;
+// const rightBorderWidth = 23;
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 5;
+// console.log(obj);
+
+
+
+// console.log(juf);
+// var juf = "Taimas";
+
+const obj = {
+    color:'red',
+    name: 'Henry',
+    age: '23',
+    isMarried: false
+
+};
+// Массив это частный случай объекта 
+// console.log(obj.name);
+// console.log(obj['name']);
+                //0      1        2      3
+// let arr = ['ds.aaas', 'dsa.sd', 'sdad', 123,];
+
+// console.log(arr[3]);
+
+
+// const result = +prompt("Are you here?", '18');
+// console.log(result + 5);
+
+
+// const answers = [];
+// answers[0] = prompt('Kak vashe imya?', '');
+// answers[1] = prompt('Kak vashe let?', '');
+// answers[2] = prompt('Kak vashe gorod?', '');
+
+// console.log(typeof(answers));
+
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+
+
+// const user = prompt('Как тебя зовут?');
+// alert(`Привет, ${user}`)
+
+
+let incr = 10,
+    decr = 10;
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
